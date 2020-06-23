d796dda3e4d7b3f90582290d983b50f475720b69
RyuByeongmin <21600220@handong.edu>
2020-06-23 19:09:24 +0900

    builder DP

--- src/main/java/hgu/csee/oodp/gps/model/Group.java
+++ src/main/java/hgu/csee/oodp/gps/model/Group.java
@@ -8,46 +8,48 @@ public class Group {
 	private String groupName;
 	private ArrayList<String> userList = new ArrayList<>();
 	private String master;
+	
 	private ArrayList<MainTask> mainTaskList = new ArrayList<>();
 	private ArrayList<Meeting> meetingList = new ArrayList<>();
 	private ArrayList<Record> recordList = new ArrayList<>();
 	
 	public Group(int id, String groupName, ArrayList<String> userList, String master) {
-		super();
 		this.id = id;
 		this.groupName = groupName;
 		this.userList = (ArrayList<String>) userList.clone();			// clone only value
 		this.master = master;
 	}
-	
+	/* Before build type
+	 * 
 	public Group(int id, String groupName, ArrayList<String> userList, String master, ArrayList<MainTask> mainTaskList) {
-		super();
-		this.id = id;
-		this.groupName = groupName;
-		this.userList = (ArrayList<String>) userList.clone();			// clone only value
-		this.master = master;
+		this(id, groupName, (ArrayList<String>)userList.clone(), master);
 		this.mainTaskList = (ArrayList<MainTask>) mainTaskList.clone();	// clone only value
 	}
 	
 	public Group(int id, String groupName, ArrayList<String> userList, String master, ArrayList<MainTask> mainTaskList, ArrayList<Meeting> meetingList) {
-		super();
-		this.id = id;
-		this.groupName = groupName;
-		this.userList = (ArrayList<String>) userList.clone();			// clone only value
-		this.master = master;
-		this.mainTaskList = (ArrayList<MainTask>) mainTaskList.clone();	// clone only value
-		this.meetingList = (ArrayList<Meeting>) meetingList.clone();
+		this(id, groupName, (ArrayList<String>)userList.clone(), master, (ArrayList<MainTask>)mainTaskList.clone());
+		this.meetingList = (ArrayList<Meeting>) meetingList.clone(); // clone only value
 	}
 	
 	public Group(int id, String groupName, ArrayList<String> userList, String master, ArrayList<MainTask> mainTaskList, ArrayList<Meeting> meetingList, ArrayList<Record> recordList) {
-		super();
-		this.id = id;
-		this.groupName = groupName;
-		this.userList = (ArrayList<String>) userList.clone();			// clone only value
-		this.master = master;
-		this.mainTaskList = (ArrayList<MainTask>) mainTaskList.clone();	// clone only value
-		this.meetingList = (ArrayList<Meeting>) meetingList.clone();
+		this(id, groupName, (ArrayList<String>)userList.clone(), master, (ArrayList<MainTask>)mainTaskList.clone, (ArrayList<Meeting>) meetingList.clone());
 		this.setRecordList((ArrayList<Record>) recordList.clone());
+	}
+	*/
+	
+	public Group buildMainTaskList(ArrayList<MainTask> mainTaskList) {
+		this.mainTaskList = (ArrayList<MainTask>)mainTaskList.clone();
+		return this;
+	}
+	
+	public Group buildMeetingList(ArrayList<Meeting> meetingList) {
+		this.meetingList = (ArrayList<Meeting>)meetingList.clone();
+		return this;
+	}
+	
+	public Group buildRecordList(ArrayList<Record> recordList) {
+		this.setRecordList((ArrayList<Record>)recordList.clone());
+		return this;
 	}
 
 	public int getId() {
